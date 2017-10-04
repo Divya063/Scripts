90764edf8a1b50bfd2cdb83f49615a198346df8d
Divya Rani <ranidivya063@gmail.com>
2017-10-04 23:28:41 +0530

    Create combinepdfs.py

---
+++ combinepdfs.py
@@ -0,0 +1,24 @@
+# combinePdfs.py - Combines all the PDFs in the current working directory into 
+
+
+import PyPDF2, os
+
+pdfFiles = []
+for filename in os.listdir('.'):
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+pdfFiles.sort()
+
+pdfWriter = PyPDF2.PdfFileWriter()
+
+for filename in pdfFiles:
+    pdfFileObj = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+    for pageNum in range(1, pdfReader.numPages):
+        pageObj = pdfReader.getPage(pageNum)
+        pdfWriter.addPage(pageObj)
+
+pdfOutput = open('example.pdf', 'wb')
+pdfWriter.write(pdfOutput)
+pdfOutput.close()
